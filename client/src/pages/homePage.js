aca2543376fa6992b6ed93fdf375c6860d27de37
Isaac Addai <30886448+itsaddai@users.noreply.github.com>
2023-05-06 15:00:28 -0400

    Update homePage.js

--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -1,7 +1,6 @@
 import HomeContent from "../components/homeContent";
 import React from 'react';
-import Footer from "../components/footer";
-import Navbar from '../components/navbar';
+
 
 export default function Home() {
     return (
