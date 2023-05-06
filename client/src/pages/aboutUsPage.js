ad79410eb378defc948fc63041b6b77780517626
Isaac Addai <30886448+itsaddai@users.noreply.github.com>
2023-05-06 14:59:17 -0400

    Update aboutUsPage.js

--- client/src/pages/aboutUsPage.js
+++ client/src/pages/aboutUsPage.js
@@ -1,11 +1,10 @@
 import AboutUs from "../components/aboutUs";
-import Footer from "../components/footer";
+
 
 export default function Home() {
     return (
         <div>
             <AboutUs />
-            <Footer />
         </div>
     );
 }
