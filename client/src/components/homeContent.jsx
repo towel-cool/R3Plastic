23a7b352e48563ab3472a01e9740ebdf8b65cdc5
Diego Diaz <79169390+towel-cool@users.noreply.github.com>
2023-05-07 10:13:59 -0400

    Update homeContent.jsx

--- client/src/components/homeContent.jsx
+++ client/src/components/homeContent.jsx
@@ -5,7 +5,7 @@ export default function HomeContent() {
   return (
     <div className="absolute top-0 left-0 bottom-0 right-0 flex justify-center items-center bg-green-700 ">
         <Link to="/aboutus">
-      <img src="./r3plastics.png" alt="R3Plastics logo" className="h-[45rem] rounded-full cursor-pointer duration-300 hover:h-[47rem]"  />
+      <img src="./R3Plastics.png" alt="R3Plastics logo" className="h-[45rem] rounded-full cursor-pointer duration-300 hover:h-[47rem]"  />
       </Link>
     </div>
   );
