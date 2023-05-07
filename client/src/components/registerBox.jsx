156c217bc8e1b93fe55b38980a18f972edaabbd9
towel-cool <daminecraft5@gmail.com>
2023-05-07 01:03:46 -0400

    Styled register box (styled for white background)

--- client/src/components/registerBox.jsx
+++ client/src/components/registerBox.jsx
@@ -45,20 +45,21 @@ export default function RegisterBox() {
     if (!success && !failure)
         return(
         <div>
-            <h3>Register</h3>
-            <form onSubmit={handleSubmit}>
-                <label>EMAIL</label>
-                <input className="text-black" value={usrEmail} type="text" required onChange={(e) => setEmail(e.target.value)} />
-                <label>PASSWORD</label>
-                <input className="text-black" value={usrPassword} type="text" required onChange={(e) => setPassword(e.target.value)} />
-                <label>FIRST NAME</label>
-                <input className="text-black" value={usrFirstName} type="text" required onChange={(e) => setFirstName(e.target.value)} />
-                <label>LAST NAME</label>
-                <input className="text-black" value={usrLastName} type="text" required onChange={(e) => setLastName(e.target.value)} />
-                <label>ADDRESS</label>
-                <div>
-                    <label>PROVINCE</label>
-                        <select className="bg-white text-black" value={usrProvince} required onChange={(e) => setProvinceName(e.target.value)}>
+            <h1 className="text-center text-3xl">Register</h1>
+            <form className="flex flex-col items-center" onSubmit={handleSubmit}>
+                <div className='flex flex-col justify-center text-center'>
+                    <label>EMAIL</label>
+                    <input className="text-black bg-gray-200 border-2 border-black" placeholder='example@example.ca' value={usrEmail} type="text" required onChange={(e) => setEmail(e.target.value)} />
+                    <label>PASSWORD</label>
+                    <input className="text-black bg-gray-200 border-2 border-black" placeholder='*********' value={usrPassword} type="text" required onChange={(e) => setPassword(e.target.value)} />
+                    <label>FIRST NAME</label>
+                    <input className="text-black bg-gray-200 border-2 border-black" placeholder='John' value={usrFirstName} type="text" required onChange={(e) => setFirstName(e.target.value)} />
+                    <label>LAST NAME</label>
+                    <input className="text-black bg-gray-200 border-2 border-black" placeholder='Doe' value={usrLastName} type="text" required onChange={(e) => setLastName(e.target.value)} />
+                </div>
+                <div className="flex flex-row justify-center items-center py-5">
+                    <label>PROVINCE: </label>
+                        <select className="bg-gray-200 text-black border-2 border-black" value={usrProvince} required onChange={(e) => setProvinceName(e.target.value)}>
                             <option value="">Select a province</option>
                             <option value="Alberta">Alberta</option>
                             <option value="British Columbia">British Columbia</option>
@@ -74,24 +75,25 @@ export default function RegisterBox() {
                             <option value="Saskatchewan">Saskatchewan</option>
                             <option value="Yukon">Yukon</option>
                         </select>
-                    <label>CITY</label>
-                        <input className="text-black" value={usrCity} type="text" required onChange={(e) => setCityName(e.target.value)} />
-                    <label>ADDRESS LINE 1</label>
-                        <input className="text-black" value={usrAddressLine1} type="text" required onChange={(e) => setAddressLine1(e.target.value)} />
+                    <label>CITY: </label>
+                        <input className="text-black bg-gray-200 border-2 border-black" placeholder='City' value={usrCity} type="text" required onChange={(e) => setCityName(e.target.value)} />
+                    <label>ADDRESS LINE 1: </label>
+                        <input className="text-black bg-gray-200 border-2 border-black" placeholder='15 Example Drive' value={usrAddressLine1} type="text" required onChange={(e) => setAddressLine1(e.target.value)} />
                 </div>
-                <input type="submit" value="Submit" />
+                <input className="bg-gray-200 rounded-md border-2 border-black cursor-pointer 
+                duration-150 hover:bg-gray-400" type="submit" value="Submit" />
             </form>
         </div>
     );
     else if (success)
         return(
-            <div>
+            <div className='text-center text-black'>
                 <h1>You are now registered!</h1>
             </div>
         );
     else if (failure)
         return(
-            <div>
+            <div className='text-center text-black'>
                 <h1>It seems there was a problem when registering...</h1>
                 <h1>{failure}</h1>
             </div>
